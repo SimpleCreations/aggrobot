b578dbea69423a86e48a4f96200314732825979e
SimpleCreations <befv@mail.ru>
2018-01-08 23:15:38 +0300

    Обновление метаданных

--- aggrobot.vpp.js
+++ aggrobot.vpp.js
@@ -1,4 +1,8 @@
 // ==VPPScript==
 // @name    AggroBot
 // @version 0.0.1
+// @script-filename aggrobot.vpp.js
+// @update-url      https://raw.githubusercontent.com/SimpleCreations/aggrobot/master/update.json
+// @script-url      https://raw.githubusercontent.com/SimpleCreations/aggrobot/master/aggrobot.vpp.js
+// @database-url    https://raw.githubusercontent.com/SimpleCreations/aggrobot/master/database.json
 // ==/VPPScript==
