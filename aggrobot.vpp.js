dccb473bca34959cc1882ff775f247e354c80819
SimpleCreations <befv@mail.ru>
2018-01-13 18:42:12 +0300

    Фикс версии

--- aggrobot.vpp.js
+++ aggrobot.vpp.js
@@ -1,6 +1,6 @@
 // ==VPPScript==
 // @name            AggroBot
-// @version         0.1.1
+// @version         0.1.2
 // @script-filename aggrobot.vpp.js
 // @update-url      https://raw.githubusercontent.com/SimpleCreations/aggrobot/master/update.json
 // @script-url      https://raw.githubusercontent.com/SimpleCreations/aggrobot/master/aggrobot.vpp.js
